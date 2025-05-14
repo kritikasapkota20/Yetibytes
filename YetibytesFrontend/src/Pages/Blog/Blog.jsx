f9360f503dca55b4ff7974d99a3a4b3ead8e123d
King Rawal <rawalrawal510@gmail.com>
2025-05-14 09:26:17 +0545

    MAY14

---
+++ YetibytesFrontend/src/Pages/Blog/Blog.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Blog from "../../Components/Blog/index"
+const BlogPage =()=>{
+    return(
+        <>
+       <Blog/>
+        </>
+    )
+}
+export default BlogPage;
